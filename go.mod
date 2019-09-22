cb0d6068a0da57a35b76744dd1eb23c004e8230d
bangn <thebangnguyen@gmail.com>
2019-09-23 00:56:31 +1000

    Set up commandline flags

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.12
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190910110746-680d30ca3117 // indirect
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.9 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/bangn/neighbor-squasher-go/cli"
+	"github.com/fatih/color"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -12,7 +14,10 @@ func main() {
 }
 
 func run(args []string, exit func(int)) {
-	app := kingpin.New(`neighbor-squasher`, `Squash repeated char in a string`)
+	app := kingpin.New(
+		`neighbor-squasher`,
+		`Squash repeated char in a string`,
+	)
 
 	app.Writer(os.Stdout)
 	app.Version(cli.VersionString())
@@ -20,4 +25,27 @@ func run(args []string, exit func(int)) {
 
 	// --------------------------
 	//  global flags
+	var (
+		squashBy string // TODO: should be rune
+		input    string
+	)
+
+	app.Flag("squash-by", "Character (UTF-8) to be squashed").
+		StringVar(&squashBy)
+
+	app.Flag("input", "input string").
+		StringVar(&input)
+
+	// --------------------------
+	// run the app, parse args
+
+	if _, err := app.Parse(args); err != nil {
+		fmt.Fprintf(os.Stderr, color.RedString("🚨 %v\n", err))
+
+		if ec, ok := err.(interface{ ExitCode() int }); ok {
+			os.Exit(ec.ExitCode())
+		} else {
+			os.Exit(1)
+		}
+	}
 }
